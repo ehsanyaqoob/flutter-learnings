15b1fe608b863d27c2c354691370c7d0e9fe36be
ehsan <ehsanyaqoob07@gmail.com>
2022-07-29 17:24:53 +0500

    1 cmt on flutter 8 hours

---
+++ lib/home-page.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = 'ehsan';
+
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+
+
+
+
+        
+        
+        title:Text('Catalog App'),
+      ),
+      body: Center(child: Container(
+        child: Text('Welcome to 30 $days of flutter by $name'),
+      ),),
+      drawer: Drawer(),
+    );
+  }
+}
+
+
+
+
+
+
+
